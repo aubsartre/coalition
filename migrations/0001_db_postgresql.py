21b72e7cec7ef75bb547bd3be62ccbb542cad485
Auberon Sartre <aubsartre@gmail.com>
2021-08-11 17:28:08 -0400

    Add Postgres migration steps 0000 and 0001
    This commit adds the nessesary files to bring a Postgres instance through the Coalition migration stages.

---
+++ migrations/0001_db_postgresql.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Add table Migrations
+# Set initial database_version
+
+steps = [
+"""
+CREATE TABLE IF NOT EXISTS Migrations(
+    database_version INT)
+""",
+"""
+INSERT INTO Migrations (database_version) VALUES (1)
+"""
+]
+
