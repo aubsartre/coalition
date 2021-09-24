883d298b32412fc7fab7dbf39e952d9f35505d56
Auberon Sartre <aubsartre@gmail.com>
2021-09-24 17:58:37 -0400

    (Housekeeping) Refactor leading tabs to spaces per PEP 8
    This commit changes leading tabs to spaces and removes various bits of trainling whitespace.
    
    To get an accurate blame, use:
        git blame -w
    
    to ignore these trivial whitespace changes.

--- migrations/0006_db_postgresql.py
+++ migrations/0006_db_postgresql.py
@@ -3,69 +3,69 @@
 steps = [
 """
 CREATE TABLE IF NOT EXISTS Jobs_deleted (
-	id SERIAL PRIMARY KEY,
-	parent INT DEFAULT 0,
-	title TEXT,
-	command TEXT,
-	dir TEXT,
-	environment TEXT,
-	state TEXT,
-	paused BOOLEAN DEFAULT '0',
-	worker TEXT,
-	start_time INT DEFAULT 0,
-	duration INT DEFAULT 0,
-	run_done INT DEFAULT 0,
-	timeout INT DEFAULT 0,
-	priority INT DEFAULT 8,
-	affinity TEXT,
-	affinity_bits BIGINT DEFAULT 0,
-	username TEXT,
-	finished INT DEFAULT 0,
-	errors INT DEFAULT 0,
-	working INT DEFAULT 0,
-	total INT DEFAULT 0,
-	total_finished INT DEFAULT 0,
-	total_errors INT DEFAULT 0,
-	total_working INT DEFAULT 0,
-	url TEXT,
-	progress FLOAT,
-	progress_pattern TEXT,
-	h_affinity BIGINT DEFAULT 0,
-	h_priority BIGINT DEFAULT 0,
-	h_paused BOOLEAN DEFAULT '0',
-	h_depth INT DEFAULT 0,
-	comments VARCHAR(255),
-	max_simultaneous_working_childs INTEGER)
+    id SERIAL PRIMARY KEY,
+    parent INT DEFAULT 0,
+    title TEXT,
+    command TEXT,
+    dir TEXT,
+    environment TEXT,
+    state TEXT,
+    paused BOOLEAN DEFAULT '0',
+    worker TEXT,
+    start_time INT DEFAULT 0,
+    duration INT DEFAULT 0,
+    run_done INT DEFAULT 0,
+    timeout INT DEFAULT 0,
+    priority INT DEFAULT 8,
+    affinity TEXT,
+    affinity_bits BIGINT DEFAULT 0,
+    username TEXT,
+    finished INT DEFAULT 0,
+    errors INT DEFAULT 0,
+    working INT DEFAULT 0,
+    total INT DEFAULT 0,
+    total_finished INT DEFAULT 0,
+    total_errors INT DEFAULT 0,
+    total_working INT DEFAULT 0,
+    url TEXT,
+    progress FLOAT,
+    progress_pattern TEXT,
+    h_affinity BIGINT DEFAULT 0,
+    h_priority BIGINT DEFAULT 0,
+    h_paused BOOLEAN DEFAULT '0',
+    h_depth INT DEFAULT 0,
+    comments VARCHAR(255),
+    max_simultaneous_working_childs INTEGER)
 """,
 """
 CREATE TABLE IF NOT EXISTS Events_deleted (
-	id SERIAL PRIMARY KEY,
-	worker VARCHAR(255),
-	job_id INT,
-	job_title TEXT,
-	state TEXT,
-	start INT,
-	duration INT)
+    id SERIAL PRIMARY KEY,
+    worker VARCHAR(255),
+    job_id INT,
+    job_title TEXT,
+    state TEXT,
+    start INT,
+    duration INT)
 """,
 """
 CREATE TABLE IF NOT EXISTS Events_archive (
-	id SERIAL PRIMARY KEY,
-	worker VARCHAR(255),
-	job_id INT,
-	job_title TEXT,
-	state TEXT,
-	start INT,
-	duration INT)
+    id SERIAL PRIMARY KEY,
+    worker VARCHAR(255),
+    job_id INT,
+    job_title TEXT,
+    state TEXT,
+    start INT,
+    duration INT)
 """,
 """
 CREATE TABLE IF NOT EXISTS Dependencies_deleted (
-	job_id Int,
-	dependency INT)
+    job_id Int,
+    dependency INT)
 """,
 """
 CREATE TABLE IF NOT EXISTS Dependencies_archive (
-	job_id Int,
-	dependency INT)
+    job_id Int,
+    dependency INT)
 """,
 """
 ALTER TABLE Jobs_archive ADD COLUMN max_simultaneous_working_childs INTEGER
